f374fcf224536b9cb1cd991a10dc255ac14ef418
Soniya <Soniya@example.com>
2024-06-16 08:48:31 +0545

    Bind a Model to View using ASP.NET Core with Jquery Ajax

--- AjaxJquery/Controllers/HomeController.cs
+++ AjaxJquery/Controllers/HomeController.cs
@@ -37,6 +37,33 @@ namespace AjaxJquery.Controllers
             int result = num1 * num2;
             return Content(result.ToString());
         }
+
+        [HttpPost]
+        public IActionResult Division(int num1, int num2)
+        {
+            if (num2 == 0)
+                return Content("Cannot divide by zero");
+
+            decimal result = (decimal)num1 / num2;
+            return Content(result.ToString());
+        }
+
+        [HttpPost]
+        public IActionResult ComputeNumber(int num1, int num2)
+        {
+            CalculationModel computeModel = new CalculationModel
+            {
+                Add = num1 + num2,
+                Substract = num1 - num2,
+                Multiply = num1 * num2,
+                Division = num2 != 0 ? (decimal)num1 / num2 : 0
+            };
+            //here returning(computeModel)model directly isnot good...we
+            //should return it with datatype "Json";
+            return Json(computeModel);
+        }
+
+
         public IActionResult Privacy()
         {
             return View();
---
+++ AjaxJquery/Models/CalculationModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AjaxJquery.Models
+{
+    public class CalculationModel
+    {
+        public int Add { get; set; }
+        public int Substract { get; set; }
+        public int Multiply { get; set; }
+        public decimal Division { get; set; }
+    }
+}
